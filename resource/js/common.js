f2dbfb1babf15ba039ad46e1101855eb3d2b885d
zz的时代 <972042562@qq.com>
2016-02-23 00:32:16 +0800

    pattern location fixed

--- resource/js/common.js
+++ resource/js/common.js
@@ -325,8 +325,8 @@ var Common = {
 
         var cloud = new THREE.PointCloud( geom, material );
         cloud.sortParticles = true;
-        var x = ( v3.x / this.w ) * this.viewW;
-        var y = ( v3.y / this.h ) * this.viewH;
+        var x = ( v3.x / this.w ) * 45;
+        var y = ( v3.y / this.h ) * 22;
         // cloud.position.set( v3.x, v3.y, 0 );
         cloud.position.set( x, y, 0 );
         cloud.scale.set( scale, scale, scale );
@@ -429,8 +429,8 @@ var Common = {
         var data = GameLevel.addPictureToBG( this.currentLocal );
         this.pattern = data.pattern;
 
-        var x = this.viewW * ((data.patPosition.x - this.pattern.geometry.parameters.width/2)*2 + this.pattern.position.x) / this.w;
-        var y = this.viewH * ((this.pattern.geometry.parameters.height/2 - data.patPosition.y)*2 + this.pattern.position.y) / this.h;
+        var x = 45 * ((data.patPosition.x - this.pattern.geometry.parameters.width/2)*2 + this.pattern.position.x) / this.w;
+        var y = 22 * ((this.pattern.geometry.parameters.height/2 - data.patPosition.y)*2 + this.pattern.position.y) / this.h;
 
         this.patPosition.set( x, y, 0 );
  
