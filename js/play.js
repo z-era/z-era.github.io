de2eff40eb9aa6a37c96b292192630d1893bfd69
bac-joker <haizekuo@gmail.com>
2019-08-06 17:39:49 +0800

    del: del index.js

--- js/play.js
+++ js/play.js
@@ -1,8 +1,7 @@
-var boom = new Boom('Plus','mainPage'),
+var boom = new Boom('Plus', 'mainPage'),
     context = boom.context,
     gameOver = false,
-
-    //score 
+    //score
     scoreShow = document.getElementById('Scores'),
     highestScores = document.getElementById('highestScore'),
     gameOverPage = document.getElementById('gameOver'),
@@ -10,11 +9,9 @@ var boom = new Boom('Plus','mainPage'),
     nowScores = document.getElementById('nowScores'),
     score = 0,
     highestScore = 0,
-
     //time
     residueTime = document.getElementById('time'),
-    
-    //number 
+    //number
     lastNumber = 0,
     nowNumber = 0,
     nowTime = 0,
@@ -22,158 +19,151 @@ var boom = new Boom('Plus','mainPage'),
     fast_interval = 201,
     slow_interval = 401,
     data_number = 0,
-    normalScores = [10,30,50,100,250,500,800,1000,3000],
-    highScores = [10,50,100,200,500,1000,1500,3000,5000],
-
+    normalScores = [10, 30, 50, 100, 250, 500, 800, 1000, 3000],
+    highScores = [10, 50, 100, 200, 500, 1000, 1500, 3000, 5000],
     //game
     velocity = 100,
     gameTime = 60,
-
     // progressbar
     loadImage = new Image(),
-
     loadButton = document.getElementById('loadButton'),
     progressbar = document.getElementById('progressbar');
-    
 
 //Function  .......................................................
 
-function windowToCanvas(x,y){
-       var bbox = context.canvas.getBoundingClientRect();
-       return {
-              x: x - bbox.left*(context.canvas.width/bbox.width),
-              y: y - bbox.top*(context.canvas.height/bbox.height)
-       };
+function windowToCanvas(x, y) {
+    var bbox = context.canvas.getBoundingClientRect();
+    return {
+        x: x - bbox.left * (context.canvas.width / bbox.width),
+        y: y - bbox.top * (context.canvas.height / bbox.height)
+    };
 }
 
-function initinalGameData(){
+function initinalGameData() {
+    boom.velocity = velocity;
+    boom.gameTime = gameTime;
+    boom.elementTime = residueTime;
 
-       boom.velocity = velocity;
-       boom.gameTime = gameTime;
-       boom.elementTime = residueTime;
-
-       context.strokeStyle  = '#d5d2d8';
-       context.fillStyle = '#918ca4';
-       boom.disapearNumContext.strokeStyle = context.strokeStyle;
-       boom.disapearNumContext.fillStyle = context.fillStyle;
-
-       
+    context.strokeStyle = '#d5d2d8';
+    context.fillStyle = '#918ca4';
+    boom.disapearNumContext.strokeStyle = context.strokeStyle;
+    boom.disapearNumContext.fillStyle = context.fillStyle;
 }
 
-function initialPlayData(){
-	     //score
-       scoreShow.innerHTML = score;
+function initialPlayData() {
+    //score
+    scoreShow.innerHTML = score;
 }
 
-function gameOverFunc(){
-	   gameOver = true;
-     boom.bombScore = [];
-     scoreIndex = 0;
+function gameOverFunc() {
+    gameOver = true;
+    boom.bombScore = [];
+    scoreIndex = 0;
 
-     gameOverPage.style.display = 'block';
-	   boom.setHeightScore(score);
-     highestScore = boom.getHightScore();
-     highestScores.innerHTML = highestScore;
-     nowScores.innerHTML = score;
-     score = 0;
-     boom.rememberNum = [];
-     boom.propLocity = [];
-     initinalGameData();
-     initialPlayData();
-     boom.start();
+    gameOverPage.style.display = 'block';
+    boom.setHeightScore(score);
+    highestScore = boom.getHightScore();
+    highestScores.innerHTML = highestScore;
+    nowScores.innerHTML = score;
+    score = 0;
+    boom.rememberNum = [];
+    boom.propLocity = [];
+    initinalGameData();
+    initialPlayData();
+    boom.start();
 }
 
-function timeFunc(){
-     if(nowTime - lastTime<fast_interval){
-            calculateMoreScore();
-     }else if(nowTime - lastTime<slow_interval){
-            calculateScore();
-     }
-     lastTime = nowTime;
+function timeFunc() {
+    if (nowTime - lastTime < fast_interval) {
+        calculateMoreScore();
+    } else if (nowTime - lastTime < slow_interval) {
+        calculateScore();
+    }
+    lastTime = nowTime;
 }
 
-function controlTime(){
-     setTimeout(function(){
-           if((+new Date()) - lastTime>slow_interval){
-                 startAgain();
-           }else{
-                 controlTime();
-           }
-     },16);
+function controlTime() {
+    setTimeout(function() {
+        if (+new Date() - lastTime > slow_interval) {
+            startAgain();
+        } else {
+            controlTime();
+        }
+    }, 16);
 }
 
-function calculateScore(){
-     score += normalScores[data_number];
-     scoreShow.innerHTML = score;
-     data_number ++;
+function calculateScore() {
+    score += normalScores[data_number];
+    scoreShow.innerHTML = score;
+    data_number++;
 }
 
-function calculateMoreScore(){
-     score += highScores[data_number];
-     scoreShow.innerHTML = score;
-     data_number ++;
+function calculateMoreScore() {
+    score += highScores[data_number];
+    scoreShow.innerHTML = score;
+    data_number++;
 }
 
-function calculateBoomScore(){
-     score += boom.bombScore;
-     scoreShow.innerHTML = score;
-     boom.bombScore =0;
+function calculateBoomScore() {
+    score += boom.bombScore;
+    scoreShow.innerHTML = score;
+    boom.bombScore = 0;
 }
 
-function startAgain(){
-     //makeTools();
-     boom.controlWitchBoom(data_number);
-     calculateBoomScore();
-     boom.slipInitial();
-     lastNumber = nowNumber = 0;
-     lastTime = nowTime = 0;
-     data_number = 0; 
+function startAgain() {
+    //makeTools();
+    boom.controlWitchBoom(data_number);
+    calculateBoomScore();
+    boom.slipInitial();
+    lastNumber = nowNumber = 0;
+    lastTime = nowTime = 0;
+    data_number = 0;
 }
 
-function makeTools(){
-     boom.controlWitchBoom(data_number);
+function makeTools() {
+    boom.controlWitchBoom(data_number);
 }
 
 //Events  ..........................................................
 
-context.canvas.onclick = function(e){
-	    e.preventDefault();
-      if(!boom.disapearing){
-             var location = windowToCanvas(e.clientX,e.clientY);
-             nowNumber = boom.getClickNum(location.x,location.y);
-             if(lastNumber===0){
-                   lastNumber=nowNumber;
-                   lastTime = nowTime = +new Date();
-                   calculateScore();
-                   controlTime();
-             }else{
-                   if(nowNumber -lastNumber !==1){
-                         lastTime = 0;
-                   }else{
-                         lastNumber = nowNumber;
-                         nowTime = +new Date();
-                         timeFunc();
-                   }
-             }   
-      }
+context.canvas.onclick = function(e) {
+    e.preventDefault();
+    if (!boom.disapearing) {
+        var location = windowToCanvas(e.clientX, e.clientY);
+        nowNumber = boom.getClickNum(location.x, location.y);
+        if (lastNumber === 0) {
+            lastNumber = nowNumber;
+            lastTime = nowTime = +new Date();
+            calculateScore();
+            controlTime();
+        } else {
+            if (nowNumber - lastNumber !== 1) {
+                lastTime = 0;
+            } else {
+                lastNumber = nowNumber;
+                nowTime = +new Date();
+                timeFunc();
+            }
+        }
+    }
 };
 
-playAgainButton.onclick = function(){
-      gameOverPage.style.display = 'none';
-      boom.gameTime = gameTime;
-      boom.pause = false;
+playAgainButton.onclick = function() {
+    gameOverPage.style.display = 'none';
+    boom.gameTime = gameTime;
+    boom.pause = false;
 };
 
-window.onblur = function(){
-	   if(!boom.pause){
-	   	       boom.togglePause();
-	   }
+window.onblur = function() {
+    if (!boom.pause) {
+        boom.togglePause();
+    }
 };
 
-window.onfocus = function(){
-	   if(boom.pause){
-	           boom.togglePause();
-	   }
+window.onfocus = function() {
+    if (boom.pause) {
+        boom.togglePause();
+    }
 };
 
 //Initial ..........................................................
@@ -183,7 +173,7 @@ context.canvas.height = document.documentElement.clientHeight - 200;
 boom.disapearNumCanvas.width = context.canvas.width;
 boom.disapearNumCanvas.height = context.canvas.height;
 
-context.font ="500 80px Helvetica";
+context.font = '500 80px Helvetica';
 context.textAlign = 'center';
 context.textBaseline = 'middle';
 
@@ -193,17 +183,21 @@ boom.disapearNumContext.textBaseline = context.textBaseline;
 
 //load  .............................................................
 
-loadButton.onclick = function(e){
+loadButton.onclick = function(e) {
     var interval,
-        percentCompleter=0,progressBar;
-     
+        percentCompleter = 0,
+        progressBar;
+
     e.preventDefault();
     loadButton.style.display = 'none';
     progressbar.style.display = 'block';
-    progressBar= new COREHTML5.Progressbar('#0611f7','#7509c1',
-    progressbar.clientWidth,progressbar.clientHeight);
+    progressBar = new COREHTML5.Progressbar(
+        '#0611f7',
+        '#7509c1',
+        progressbar.clientWidth,
+        progressbar.clientHeight
+    );
     progressBar.appendTo(progressbar);
-
 
     boom.queueImage('../img/prop41.png');
     boom.queueImage('../img/prop42.png');
@@ -220,24 +214,20 @@ loadButton.onclick = function(e){
     boom.queueImage('../img/prop91.png');
     boom.queueImage('../img/prop92.png');
 
-    interval = setInterval(function(){
-            percentCompleter = boom.loadImages();
-            if(percentCompleter===100){
-                  clearInterval(interval);
-                  progressbar.style.display = 'none';
+    interval = setInterval(function() {
+        percentCompleter = boom.loadImages();
+        if (percentCompleter === 100) {
+            clearInterval(interval);
+            progressbar.style.display = 'none';
 
-                  setTimeout(function(){
-                          document.getElementById('loadPage').style.display = 'none';
-                          initinalGameData();
-                          initialPlayData();
-                          boom.start();
-                  },400);
-            }
-            progressBar.erase();
-        progressBar.draw(percentCompleter/100);
-    },16);
-               
+            setTimeout(function() {
+                document.getElementById('loadPage').style.display = 'none';
+                initinalGameData();
+                initialPlayData();
+                boom.start();
+            }, 400);
+        }
+        progressBar.erase();
+        progressBar.draw(percentCompleter / 100);
+    }, 16);
 };
-
-
-
